62388e42fd0cd3f01b6b585545adf128d09e79ea
ismailjosim <ismailjosim99@gmail.com>
2024-10-07 14:34:57 +0600

    like button and single pet details modal completed

--- app.js
+++ app.js
@@ -48,7 +48,9 @@ async function fetchSingleCategories(category) {
             cardsContainerEl.innerHTML = `
             <div class="col-span-3 flex gap-5 flex-col justify-center items-center">
                     <img src="./assets/no-data.png" alt="">
-                    <h3 class="text-3xl font-black">No data found ⚠️</h3>
+                    <h3 class="text-3xl font-black">No Information Available</h3>
+                    <p class="sm:w-1/2 w-full text-center mx-auto font-medium">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a.
+                    </p>
             </div>`
         } else {
             pets.forEach((item) => {
@@ -70,18 +72,20 @@ async function fetchSingleCategories(category) {
                         <img src=${ image }
                             alt=${ pet_name } class="rounded-xl" />
                     </figure>
+
                     <div class="card-body">
                         <h2 class="card-title text-3xl">Name: ${ pet_name }</h2>
                         <p>
                             <i class="fa-solid fa-border-all"></i>
                             Breed:
-                            <span>${ breed }</span>
+                            <span>${ breed ? breed : 'Not Available' }</span>
                         </p>
                         <p>
                             <i class="fa-regular fa-calendar"></i>
                             Birth:
                             <span>${ date_of_birth ? date_of_birth : 'Not Available'
-                    }</span>
+                    }
+                    </span>
                         </p>
                         <p>
                             <i class="fa-solid fa-mercury"></i>
@@ -96,14 +100,14 @@ async function fetchSingleCategories(category) {
                         <div class="divider"></div>
                         <div class="card-actions flex justify-between">
                             <button
-                            onclick="likePet(${ petId })"
-                             class="btn px-5 bg-transparent text-secondary border-primary text-2xl"><i
-                                    class="fa-solid fa-thumbs-up"></i></button>
+                            onclick="likePet('${ image }',this)"
+                             class="btn px-5 bg-transparent text-secondary border-primary text-2xl">
+                             <i class="fa-solid fa-thumbs-up"></i></button>
                             <button
                             onclick="adoptPet(${ petId })"
                                 class="btn border border-primary bg-transparent text-primary font-bold px-5 text-xl">Adopt</button>
                             <button
-                            onclick="adoptPet(${ petId })"
+                            onclick="showDetails(${ petId })"
                                 class="btn border border-primary bg-transparent text-primary font-bold px-5 text-xl">Details</button>
                         </div>
                     </div>
@@ -111,25 +115,20 @@ async function fetchSingleCategories(category) {
         `
             })
         }
-    }, 2000)
+    }, 20) // TODO: need to change 2000
 }
-async function likePet(id) {
-    console.log(pet)
-    // const {
-    //     petId,
-    //     breed,
-    //     category,
-    //     date_of_birth,
-    //     price,
-    //     image,
-    //     gender,
-    //     pet_details,
-    //     vaccinated_status,
-    //     pet_name,
-    // } = item || {}
+const likeContainerEl = document.getElementById('like_container')
+likeContainerEl.innerHTML = ''
+
+async function likePet(img, e) {
+    e.classList.add("bg-green-800", "text-white");
+    likeContainerEl.innerHTML += `
+    <div class="rounded-lg border-2 h-28 flex justify-center items-center shadow-md">
+         <img class="rounded-lg p-1" src=${ img } alt="">
+    </div>`
 }
+
 async function adoptPet(pet) {
-    console.log(pet)
     // const {
     //     petId,
     //     breed,
@@ -143,20 +142,60 @@ async function adoptPet(pet) {
     //     pet_name,
     // } = item || {}
 }
-async function showDetails(pet) {
-    console.log(pet)
-    // const {
-    //     petId,
-    //     breed,
-    //     category,
-    //     date_of_birth,
-    //     price,
-    //     image,
-    //     gender,
-    //     pet_details,
-    //     vaccinated_status,
-    //     pet_name,
-    // } = item || {}
+// document.getElementById('my_modal_5').showModal()
+async function showDetails(id) {
+    const res = await fetch(
+        `https://openapi.programming-hero.com/api/peddy/pet/${ id }`,
+    )
+    const data = await res.json()
+    console.log(data)
+
+    const {
+        breed,
+        category,
+        date_of_birth,
+        price,
+        image,
+        gender,
+        pet_details,
+        vaccinated_status,
+        pet_name,
+    } = data.petData || {}
+    document.getElementById('my_modal_5').showModal()
+    const modalContentEl = document.getElementById('modal_content')
+    modalContentEl.innerHTML = `
+                        <figure class="px-5 pt-5 ml-10">
+                            <img src=${ image } alt=${ pet_name } class="rounded-xl" />
+                        </figure>
+                        <h3 class="text-3xl font-bold my-5">${ pet_name }</h3>
+                        <div class="grid grid-cols-2 justify-between gap-2 ">
+                            <p class="text-lg font-bold">
+                                <i class="fa-solid fa-border-all"></i>
+                                Breed: <span>${ breed ? breed : 'Not Available' }</span>
+                            </p>
+                            <p class="text-lg font-bold">
+                                <i class="fa-regular fa-calendar"></i>
+                                Birth:
+                                <span>
+                                ${ date_of_birth ? date_of_birth : 'Not Available' }
+                                </span>
+                            </p>
+                            <p class="text-lg font-bold">
+                                <i class="fa-solid fa-mercury"></i>
+                                Gender: <span>${ gender ? gender : 'Not Available' }</span>
+                            </p>
+                            <p class="text-lg font-bold">
+                                <i class="fa-solid fa-virus"></i>
+                                Price: <span >$${ price ? price : 'Not Available' }</span>
+                            </p>
+                            <p class="text-lg font-bold">
+                                <i class="fa-solid fa-dollar-sign"></i>
+                                Vaccinated Status: <span>${ vaccinated_status }</span>
+                            </p>
+                        </div>
+                        <div class="divider"></div>
+                        <h3 class="text-2xl font-bold">Details Information</h3>
+                        <p class="pt-2">${ pet_details }</p>`
 }
 
 fetchCategories()
