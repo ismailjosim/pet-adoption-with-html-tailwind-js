7c38e36cdee0a8074f189db238ed4f9c339b6255
ismailjosim <ismailjosim99@gmail.com>
2024-10-07 12:46:40 +0600

    show all pets data when button is not clicked

--- app.js
+++ app.js
@@ -1,48 +1,69 @@
-
 // * fetch categories data
 async function fetchCategories() {
-    const res = await fetch("https://openapi.programming-hero.com/api/peddy/categories");
-    const data = await res.json();
-    showCategories(data.categories);
-
+    const res = await fetch(
+        'https://openapi.programming-hero.com/api/peddy/categories',
+    )
+    const data = await res.json()
+    showCategories(data.categories)
 }
 
 // show Categories Data
 async function showCategories(categories) {
-    const categoriesEl = document.getElementById("categories");
-    categories.forEach(item => {
+    const categoriesEl = document.getElementById('categories')
+    categories.forEach((item) => {
         categoriesEl.innerHTML += `<button onclick="fetchSingleCategories(this)" class="flex items-center justify-center gap-5 border-2 px-10 py-3 rounded-lg hover:rounded-full transition-transform ease-in-out">
-            <img src=${ item.
-                category_icon } alt="">
+            <img src=${ item.category_icon } alt="">
                     <h2 class="text-2xl font-bold">${ item.category }</h2>
                 </button>`
-    });
-
+    })
 }
 
-const cardsContainerEl = document.getElementById("cards_container")
+const cardsContainerEl = document.getElementById('cards_container')
 
 // single category data
 async function fetchSingleCategories(category) {
     cardsContainerEl.innerHTML = `
                 <div class="col-span-3 flex justify-center items-center">
                   <span class="loader"></span>
-                </div>`;
-    const name = category.innerText;
-    const res = await fetch(`https://openapi.programming-hero.com/api/peddy/category/${ name }`);
-    const data = await res.json();
+                </div>`
+    const name = category.innerText
+    let pets
+    if (name) {
+        const res = await fetch(
+            `https://openapi.programming-hero.com/api/peddy/category/${ name }`,
+        )
+        const data = await res.json()
+        pets = data.data
+    } else {
+        const res = await fetch(
+            `https://openapi.programming-hero.com/api/peddy/pets`,
+        )
+        const data = await res.json()
+        pets = data.pets
+    }
 
     setTimeout(() => {
-        cardsContainerEl.innerHTML = "";
-        if (data.data.length === 0) {
+        cardsContainerEl.innerHTML = ''
+        if (pets.length === 0) {
             cardsContainerEl.innerHTML = `
             <div class="col-span-3 flex gap-5 flex-col justify-center items-center">
                     <img src="./assets/no-data.png" alt="">
                     <h3 class="text-3xl font-black">No data found ⚠️</h3>
-            </div>`;
+            </div>`
         } else {
-            data.data.forEach(item => {
-                const { petId, breed, category, date_of_birth, price, image, gender, pet_details, vaccinated_status, pet_name } = item || {}
+            pets.forEach((item) => {
+                const {
+                    petId,
+                    breed,
+                    category,
+                    date_of_birth,
+                    price,
+                    image,
+                    gender,
+                    pet_details,
+                    vaccinated_status,
+                    pet_name,
+                } = item || {}
                 cardsContainerEl.innerHTML += `
         <div class="card bg-base-100 w-full shadow-xl">
                     <figure class="px-10 pt-10">
@@ -59,12 +80,13 @@ async function fetchSingleCategories(category) {
                         <p>
                             <i class="fa-regular fa-calendar"></i>
                             Birth:
-                            <span>${ date_of_birth ? date_of_birth : "Not Available" }</span>
+                            <span>${ date_of_birth ? date_of_birth : 'Not Available'
+                    }</span>
                         </p>
                         <p>
                             <i class="fa-solid fa-mercury"></i>
                             Gender:
-                            <span>${ gender ? gender : "Not Available" }</span>
+                            <span>${ gender ? gender : 'Not Available' }</span>
                         </p>
                         <p>
                             <i class="fa-solid fa-dollar-sign"></i>
@@ -82,16 +104,12 @@ async function fetchSingleCategories(category) {
                         </div>
                     </div>
                 </div>
-        `;
-            });
+        `
+            })
         }
-    }, 2000);
-
-
+    }, 2000)
 }
-
-
 
 fetchCategories()
 
-
+fetchSingleCategories('')
